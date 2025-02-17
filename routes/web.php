5ab1cc5b58b7b88ed376dc1380c38a9f34b19836
Jessica Amelia <ameliajessica997@gmail.com>
2025-02-17 13:32:16 +0700

    Routing

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,36 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return 'Hello World';
+ });
+ 
+ Route::get('/world', function () {
+    return 'World';
+ });
+
+ Route::get('/selamat', function () {
+    return 'Selamat Datang';
+ });
+ 
+ Route::get('/about', function () {
+    return 'Nim : 2341760185 <br> Nama : Jessica Amelia';
+ });
+
+ Route::get('/user/{name}', function ($name) {
+    return 'Nama saya '.$name;
+    });
+    
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID '.$id;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+    });
+    
